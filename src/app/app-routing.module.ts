3ef66b7348b62eb7992ef9c9abfa9ce0ce21b3ba
zap-wada <ha_wada@argon.co.jp>
2019-04-03 12:41:28 +0900

    modify routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './component/page/index/index.component';
+import { TestComponent } from './component/page/test/test.component';
 
-const routes: Routes = [{ path: '', component: IndexComponent }];
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'test', component: TestComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
