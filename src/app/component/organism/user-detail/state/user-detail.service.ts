d3b40b6908a60def472193d03d410c8596b0894d
zap-wada <ha_wada@argon.co.jp>
2019-05-29 19:08:39 +0900

    modify user-detail organism service

--- src/app/component/organism/user-detail/state/user-detail.service.ts
+++ src/app/component/organism/user-detail/state/user-detail.service.ts
@@ -8,6 +8,7 @@ export class UserDetailService {
   constructor(private userDetailStore: UserDetailStore, private apiService: ApiService) {}
 
   get(userId: number) {
+    this.reset();
     this.apiService.getUser(userId).subscribe(content => {
       this.userDetailStore.set([content]);
     });
