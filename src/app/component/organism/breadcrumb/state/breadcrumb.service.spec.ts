0cc43b7b8c4d60177a9b0451e3b8e749e58dd05e
zap-wada <ha_wada@argon.co.jp>
2019-05-31 13:19:09 +0900

    add test config

--- src/app/component/organism/breadcrumb/state/breadcrumb.service.spec.ts
+++ src/app/component/organism/breadcrumb/state/breadcrumb.service.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { Breadcrumb, createBreadcrumb } from './breadcrumb.model';
 import { BreadcrumbService } from './breadcrumb.service';
 import { BreadcrumbStore } from './breadcrumb.store';
 
@@ -19,5 +20,22 @@ describe('BreadcrumbService', () => {
 
   it('should be created', () => {
     expect(breadcrumbService).toBeDefined();
+  });
+
+  describe('set', () => {
+    it('should return dummy value from spy', () => {
+      const spy = spyOn(breadcrumbStore, 'set');
+      const dummyBreadcrumb = createBreadcrumb({ title: 'test' });
+      breadcrumbService.set([dummyBreadcrumb]);
+      expect(spy).toHaveBeenCalledWith([dummyBreadcrumb]);
+    });
+  });
+
+  describe('reset', () => {
+    it('should return empty array from spy', () => {
+      const spy = spyOn(breadcrumbStore, 'set');
+      breadcrumbService.reset();
+      expect(spy).toHaveBeenCalledWith([]);
+    });
   });
 });
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -18,7 +18,15 @@ module.exports = function(config) {
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage/sample'),
       reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      fixWebpackSourcePaths: true,
+      thresholds: {
+        global: {
+          statements: 80,
+          lines: 80,
+          branches: 80,
+          functions: 80
+        }
+      }
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
